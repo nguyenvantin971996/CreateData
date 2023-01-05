830f0e00d3f90b3d38a3e8c3f61a855f40fdfa5f
nguyenvantinrsreu@gmail.com <nguyenvantinrsreu@gmail.com>
2023-01-05 22:43:27 +0300

    05/01/2023

--- Data/concat_csv.py
+++ Data/concat_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df_homes = pd.read_csv("data_18_18_yen.csv")
-df_homes1 = pd.read_csv("data_18_18_yen_2.csv")
-df_homes2 = pd.read_csv("data_18_18_yen_3.csv")
+df_homes = pd.read_csv("data_10_node_yen.csv")
+df_homes1 = pd.read_csv("data_10_node_yen_1.csv")
+df_homes2 = pd.read_csv("data_10_node_yen_2.csv")
 
-pd.concat([df_homes, df_homes1, df_homes2]).to_csv('data_18_yen_2.csv', index=False)
+pd.concat([df_homes, df_homes1, df_homes2]).to_csv('data_10_node_yen.csv', index=False)
